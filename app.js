62d61ad39e9c1869caa3ad34a8160f4084c53b99
hugh-webdev <120143722+hugh-webdev@users.noreply.github.com>
2024-02-05 21:31:16 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+    // allows navbar to transistion on scroll
+    $(function () {
+        $(document).scroll(function () {
+          var $nav = $("#mainNavbar");
+          $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+        });
+      });
+  
+      ////// Smooth Scroll Into View JvaScript Function
+      document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+  
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+          });
+        });
+      });
+  
+      window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+      }
+  
+
+
+      //// Function to allow boxes to scroll into view
+    const boxes = document.querySelectorAll('.box');
+
+    window.addEventListener('scroll', checkBoxes);
+    
+    checkBoxes();
+    
+    function checkBoxes(){
+       const triggerBottom = window.innerHeight / 5 * 4;
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+        if(boxTop < triggerBottom){
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    });
+    }
+
+    
